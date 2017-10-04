f1497f831ee399734fa4475ef28ec96ae6c130b1
Steffen Wendzel <wendzel@hs-worms.de>
2017-10-04 13:39:07 +0200

    use more conservative settings

--- nel.h
+++ nel.h
@@ -47,9 +47,9 @@
 				"(C) 2017 Steffen Wendzel (wendzel (at) hs-worms (dot) de), Network Security Research Group/ZTT, Worms University of Applied Sciences, www.wendzel.de\n" \
 				"Version " TOOL_VERSION "\n\n"
 
-#define CR_NEL_TESTPKT_WAITING_TIME	7 /* Waiting time of NEL receiver for packets from Alice (in seconds) */
-#define NUM_COMM_PHASE_PKTS		1000  /* number of COMM phase packets to send; should be enough to succeed also under heavily-blocked circumstances */
-#define NUM_OVERALL_REQ_PKTS	400   /* number of CC packets (overall) that must go through warden before we count NEL as completed */
+#define CR_NEL_TESTPKT_WAITING_TIME	5 /* Waiting time of NEL receiver for packets from Alice (in seconds) */
+#define NUM_COMM_PHASE_PKTS		3000  /* number of COMM phase packets to send; should be enough to succeed also under heavily-blocked circumstances */
+#define NUM_OVERALL_REQ_PKTS		200   /* number of CC packets (overall) that must go through warden before we count NEL as completed */
 #define NUM_COMM_PHASE_SND_PKTS_P_PROT	4 /* how many packets to send during the communication phase per non-blocked protocol in a row */
 
 
