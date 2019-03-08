5aa560ed5280a945b6ed98a2d680563ed42dec6a
Steffen Wendzel <wendzel@hs-worms.de>
2019-03-08 12:57:23 +0100

    some line-breaks/formating

--- nel.h
+++ nel.h
@@ -44,14 +44,20 @@
 #define TOOL_VERSION		"0.2.4"
 #define WELCOME_MESSAGE		"NEL: Implementation of a Network Environment Learning (NEL) Phase\n" \
 				"     for Network Covert Channel Research\n\n" \
-				"(C) 2017-2018 Steffen Wendzel (wendzel (at) hs-worms (dot) de), Network Security Research Group/ZTT, Worms University of Applied Sciences, www.wendzel.de\n" \
+				"(C) 2017-2019 Steffen Wendzel (wendzel (at) hs-worms (dot) de), " \
+				"Network Security Research Group (NSRG), " \
+				"Worms University of Applied Sciences, " \
+				"WWW: www.wendzel.de\n" \
 				"Version " TOOL_VERSION "\n\n"
 
-#define CR_NEL_TESTPKT_WAITING_TIME	5 /* Waiting time of NEL receiver for packets from Alice (in seconds) */
-#define NUM_COMM_PHASE_PKTS		3000  /* number of COMM phase packets to send; should be enough to succeed also under heavily-blocked circumstances */
-#define NUM_OVERALL_REQ_PKTS		200   /* number of CC packets (overall) that must go through warden before we count NEL as completed */
-#define NUM_COMM_PHASE_SND_PKTS_P_PROT	5 /* how many packets to send during the *COMM* phase per non-blocked protocol in a row */
-#define NUM_NEL_TESTPKT_SND_PKTS_P_PROT 5 /* how many packets to be sent per CC type during *NEL* phase */
+#define CR_NEL_TESTPKT_WAITING_TIME	5	/* Waiting time of NEL receiver for packets from Alice (in sec) */
+#define NUM_COMM_PHASE_PKTS		3000	/* number of COMM phase packets to send; should be enough to 
+						 * succeed also under heavily-blocked circumstances */
+#define NUM_OVERALL_REQ_PKTS		200	/* number of CC packets (overall) that must go through warden
+						 * before we count NEL as completed */
+#define NUM_COMM_PHASE_SND_PKTS_P_PROT	5	/* how many packets to send during the *COMM* phase per non-blocked
+						 * protocol in a row */
+#define NUM_NEL_TESTPKT_SND_PKTS_P_PROT 5	/* how many packets to be sent per CC type during *NEL* phase */
 
 
 #define MODE_UNSET		0x00
@@ -66,10 +72,10 @@
 #define min(a, b)		(a < b ? a : b)
 
 typedef struct {
-	u_int32_t			announced_proto;
+	u_int32_t		announced_proto;
 #define RESULT_RECVD		0x01
-#define RESULT_TIMEOUT		0x00 /* not received during time-slot */
-	u_int32_t			result;
+#define RESULT_TIMEOUT		0x00		/* not received during time-slot */
+	u_int32_t		result;
 } nel_proto_t;
 
 void *cs_COMM_sender(void *);
@@ -77,5 +83,4 @@ void *cs_NEL_handler(void *);
 void *cr_NEL_handler(void *);
 void *cr_measure(void *);
 void usage(void);
-
 
